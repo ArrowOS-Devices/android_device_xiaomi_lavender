0731966c22432e2e765d7a5966872a6aa63389a1
Ratoriku <a1063021545@gmail.com>
2021-04-26 22:32:11 +0800

    lavender: Switch to walleye oreo fp
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -28,6 +28,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.0.1.0.QFGMIXM release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
-BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.0.1.0.QFGMIXM:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
