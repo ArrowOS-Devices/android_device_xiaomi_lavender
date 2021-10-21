dbc25f6d2771fcdb30ed1428a0e51f1dd672e190
Aaradhay Vijay <80588995+inferno964@users.noreply.github.com>
2021-10-21 20:50:12 +0530

    aosp_lavender.mk

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -9,8 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelExperience stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -18,10 +20,10 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := aosp_lavender
 PRODUCT_MODEL := Redmi Note 7
 
-DEVICE_MAINTAINER := Ratoriku, Hazama25
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
